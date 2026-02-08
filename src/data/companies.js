55c52f08038c31d492c5451e77409176b441b4f1
khanempire <mdaamirkhan7186@gmail.com>
2026-02-08 13:16:30 +0530

    Update Experience component styles and fix image paths in companies data

--- src/data/companies.js
+++ src/data/companies.js
@@ -28,7 +28,7 @@ export const companies = [
     location: "Bengaluru, India",
     description:
       "Worked on an advanced Fleet Orchestration Platform (FOP) that digitizes and automates commercial fleet operations for both passenger and cargo transportation. Built a scalable, real-time Notification Center to streamline system-wide communications. Implemented Driver Management features to efficiently manage driver roles, permissions, and vehicle assignments, including seamless vehicle linking and unlinking. Developed a mobile application for drivers to connect with assigned vehicles and track trip and journey details in real time.",
-    bannerImage: "../../public/images/ZF_Group.jpg",
+    bannerImage: "/images/ZF_Group.jpg",
   },
   {
     id: 3,
@@ -38,6 +38,6 @@ export const companies = [
     location: "Bengaluru, India",
     description:
       "Built a cloud provisioning marketplace to accelerate DevOps adoption by enabling teams to quickly spin up ready-to-use development platforms. The platform allows users to choose their preferred cloud provider and predefined tool cartridges (curated sets of DevOps tools), automatically provisioning a complete DevOps environment within 30 minutes. This significantly reduced setup time and enabled teams to immediately start building and delivering DevOps-driven products.",
-    bannerImage: "../../public/images/accenture.jpg",
+    bannerImage: "/images/accenture.jpg",
   },
 ];
